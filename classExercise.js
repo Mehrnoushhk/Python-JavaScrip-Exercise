c06255b0625002266de067fc5e598e7bc9a841ed
Mehrnoushhk <mehrnoosh.haghighat@gmail.com>
2020-12-20 00:14:08 +0330

    Solve Example2

--- classExercise.js
+++ classExercise.js
@@ -1,3 +1,4 @@
+// * Example 1
 class Dog {
     constructor(dogName, birthDate){
         this.dogName = dogName;
@@ -9,6 +10,54 @@ class Dog {
         return date.getFullYear() - this.birthDate;
     }
 }
-myDog = new Dog('Lucy', 1982);
-console.log('My dog Name is ' + myDog.dogName);
-console.log('She is ' + myDog.age() + ' years old');
+// myDog = new Dog('Lucy', 1982);
+// console.log('My dog Name is ' + myDog.dogName);
+// console.log('She is ' + myDog.age() + ' years old');
+
+// * Example 2
+// * Define car object with 2 properties (speed, brand, model) and 3 methods (breake, accelerate, describe, decelerate)
+
+class Car {
+    constructor(brand, model) {
+        this.brand = brand;
+        this.model = model;
+        this.speed = 0;
+        this.motion = 'The car is at rest'
+    }
+    motionCheck() {
+        if (this.speed > 0) {
+            this.motion = 'The car is moving';
+        }
+        else {
+            this.motion = 'The car is at rest';
+        }
+    }
+    accelerate(amount) {
+        this.speed += amount;
+        this.motionCheck();
+    }
+    decelerate(amount) {
+        if (amount <= this.speed) {
+            this.speed -= amount;
+        } else {
+            this.speed = 0;
+        }
+        this.motionCheck();
+    }
+    break() {
+        this.speed = 0;
+        this.motionCheck();
+    }
+    describe() {
+        console.log(`The car brand is ${this.brand} and its model is ${this.model} ${this.motion}`)
+    }
+
+}
+myCar = new Car('BMW', 'X6');
+myCar.describe();
+myCar.accelerate(60);
+console.log(myCar.describe());
+myCar.decelerate(40);
+console.log(myCar.describe());
+myCar.break();
+console.log(myCar.describe());
--- classExercise.py
+++ classExercise.py
@@ -1,3 +1,5 @@
+
+# * Example 1
 class Dog():
     def __init__(self, name, age):
         self.name = name
@@ -7,7 +9,50 @@ class Dog():
     def rollOver(self, ntimes):
         print('{} is rolled over {} times'.format(self.name.title(), str(ntimes)))
 
-myDog = Dog('drago', 2)
-print('{} is {} years old'.format(myDog.name.title(), str(myDog.age)))
-myDog.sit()
-myDog.rollOver(5)
+# myDog = Dog('drago', 2)
+# print('{} is {} years old'.format(myDog.name.title(), str(myDog.age)))
+# myDog.sit()
+# myDog.rollOver(5)
+
+# * Example 2
+# * Define car object with 2 properties (speed, brand, model) and 3 methods (breake, accelerate, describe, decelerate)
+
+class Car():
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        self.speed = 0
+        self.motion = 'The car is at rest'
+    def motionCheck(self):
+        if self.speed > 0:
+            self.motion = 'The car is moving'
+        else:
+            self.motion = 'The car is at rest'
+    def accelerate(self, amount):
+        self.speed += amount
+        self.motionCheck()
+    def decelerate(self, amount):
+        if amount <= self.speed:
+            self.speed -= amount
+        else:
+            self.speed = 0
+        self.motionCheck()
+    def breake(self):
+        self.speed = 0
+        self.motionCheck()
+    def describe(self):
+        print('My car brand is {} and its model is {} and {}'.format(self.brand, self.model, self.motion))
+
+
+myCar = Car('BMW', 'X6')
+print(myCar.describe())
+myCar.accelerate(100)
+print('Speed is {}'.format(str(myCar.speed)))
+print(myCar.describe())
+myCar.decelerate(60)
+print('Speed is {}'.format(str(myCar.speed)))
+print(myCar.describe())
+myCar.breake()
+print('Speed is {}'.format(str(myCar.speed)))
+print(myCar.describe())
+
